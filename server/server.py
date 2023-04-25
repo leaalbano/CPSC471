998ddda502198cf0daa8a0c745806b62b160f6c3
Eulices Martinez De la Cruz <eulicescl2004@gmail.com>
2023-04-24 22:12:40 -0700

    Adding client code, adding Server code

---
+++ server/server.py
@@ -0,0 +1,30 @@
+import socket
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.host, self.port))
+
+    def start(self):
+        self.socket.listen()
+        print('Server listening on {}:{}'.format(self.host, self.port))
+        conn, addr = self.socket.accept()
+        print('Connected by', addr)
+
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            received_message = data.decode()
+            print('Received message:', received_message)
+
+            send_message = input('Enter a message to send: ')
+            conn.sendall(send_message.encode())
+
+        conn.close()
+
+if __name__ == '__main__':
+    server = Server('localhost', 8000)
+    server.start()
