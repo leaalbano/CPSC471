90a6ad1bb819277611f26749a68916fffbefb3b0
Eulices Martinez De la Cruz <eulicescl2004@gmail.com>
2023-04-26 05:17:00 -0700

    Removing unnecessary comments in receive.py

--- common/receive.py
+++ common/receive.py
@@ -3,17 +3,11 @@ class ReceiveFile:
         self.size = size
         self.data = firstChunk
         self.bytes_received = len(firstChunk)
-        print("Received message:", firstChunk)
-        print("Current size:", self.bytes_received)
-        print("Total size:", self.size)
 
     def receive_file(self, client_socket):
         while self.bytes_received < self.size:
             buffer = client_socket.recv(80)
             self.data += buffer.decode()
-            print("Received message:", buffer.decode())
-            print("Current size:", self.bytes_received)
-            print("Total size:", self.size)
             self.bytes_received += len(buffer)
 
     def get_data(self):
